c78d66a8c4a63f8af964ebecfcf3d710e85d88a5
Tristan Sloughter <tristan.sloughter@gmail.com>
2011-07-23 17:07:44 -0500

    removed all but known working formulas for certain Ubuntus -- hehe, thats just erlang for now

--- Library/Formula/youtube-dl.rb
+++
@@ -1,8 +0,0 @@
-require 'formula'
-
-class YoutubeDl < ScriptFileFormula
-  url 'https://github.com/rg3/youtube-dl/raw/2011.03.29/youtube-dl'
-  homepage 'http://rg3.github.com/youtube-dl/'
-  md5 'd481c3e15e306e3e5157e0142758c011'
-  version '2011.03.29'
-end
